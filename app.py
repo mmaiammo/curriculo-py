227e6a89e752df78007ece9265b12314de12acba
Maia <mmaia.inf@gmail.com>
2022-04-01 22:43:52 -0300

    Realocação do app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from databases import dbmongo
 
-from .routes.main import main
+from routes.main import main
 
 
-def create_app(config_object="app.settings"):
+def create_app(config_object="settings"):
     app = Flask(__name__)
     app.config.from_object(config_object)
     app.register_blueprint(main)
