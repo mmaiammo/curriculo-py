01baa844411b01d81749067f0a312a800695eaec
Maia <mmaia.inf@gmail.com>
2022-04-02 11:11:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ from routes.main import main
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(".settings.py")
+    app.config.from_object("settings.py")
     app.register_blueprint(main)
 
     # config_object = "settings"
-    dbmongo.init_app(app)
-    return app
+    # dbmongo.init_app(app)
+    # return app
 
 
 if __name__ == "__main__":
