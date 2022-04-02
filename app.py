04be61659ff302ca25d85971ea22c571d1e4367d
Maia <mmaia.inf@gmail.com>
2022-04-02 12:21:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
-from databases import dbmongo
+# from databases import dbmongo
 
 from routes.main import main
 
+app = Flask(__name__)
+app.register_blueprint(main)
 
-def create_app(config_object="settings"):
-    app = Flask(__name__)
-    app.config.from_object(config_object)
-    app.register_blueprint(main)
-
-    dbmongo.init_app(app)
-    return app
+if __name__ == "__main__":
+    app.run(debug=True)
