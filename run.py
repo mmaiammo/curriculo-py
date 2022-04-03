93f4ca24d4fd4a281e2e644f5740ae1030c1626c
Maia <mmaia.inf@gmail.com>
2022-04-02 22:52:58 -0300

    Ignore

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from flask import Flask
-# from .extensions import mongo
-#
+
 from main import main
 
 from flask_pymongo import PyMongo
@@ -9,12 +8,3 @@ mongo = PyMongo()
 app = Flask(__name__)
 app.config.from_object('settings')
 app.register_blueprint(main)
-# mongo.init_app(app)
-
-
-# def create_app():
-#     return app
-
-
-# if __name__ == "__main__":
-#     create_app().run()
