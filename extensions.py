028bfffa4ce27f6b48123d820f9ad58be64e0c71
Maia <mmaia.inf@gmail.com>
2022-04-03 11:20:04 -0300

    Fix

--- extensions.py
+++
@@ -1,6 +0,0 @@
-from flask_pymongo import PyMongo
-mongo = PyMongo()
-
-
-def init_app(app):
-    return mongo.init_app(app)
--- run.py
+++ run.py
@@ -1,10 +1,24 @@
 from flask import Flask
-
-from main import main
-
-from flask_pymongo import PyMongo
-mongo = PyMongo()
+from flask import render_template
 
 app = Flask(__name__)
-app.config.from_object('settings')
-app.register_blueprint(main)
+app.config.from_object('config.DevelopmentConfig')
+
+
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+
+@app.route('/novo')
+def novoCurriculo():
+    return render_template('curriculo.html')
+
+
+@app.route('/novo', methods=["POST"])
+def inserirCurriculo():
+    return render_template('curriculo.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
