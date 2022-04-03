f62461415660616c13ea12c8830e7da0c0b6b8b0
Maia <mmaia.inf@gmail.com>
2022-04-02 22:26:33 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,23 +1,20 @@
 from flask import Flask
 # from .extensions import mongo
 #
-
 from main import main
 
 from flask_pymongo import PyMongo
 mongo = PyMongo()
 
-
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('settings')
-
-    mongo.init_app(app)
-
-    app.register_blueprint(main)
-
-    return app
+app = Flask(__name__)
+app.config.from_object('settings')
+app.register_blueprint(main)
+mongo.init_app(app)
 
 
-if __name__ == "__main__":
-    create_app().run()
+# def create_app():
+#     return app
+
+
+# if __name__ == "__main__":
+#     create_app().run()
