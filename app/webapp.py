0801658ad5df00800b2d93d4947e24abb85e113b
Maia <mmaia.inf@gmail.com>
2022-04-11 13:05:15 -0300

    Refactoring

--- app/webapp.py
+++ app/webapp.py
@@ -1,7 +1,7 @@
 import requests
 # type: ignore
 # import json
-
+from flask import current_app
 from flask import Blueprint
 from flask import render_template
 
@@ -15,6 +15,31 @@ def index():
 
 @webapp.route('/list')
 def list():
+    response = requests.get(current_app.config["API_MONGO"])
+    curriculos = response.json()
+    return render_template('curriculo/list.html', curriculos=curriculos)
+
+
+@webapp.route('/list/<int:page_id>')
+def listone(page_id):
+    # , "?/:62502f5a559389bd6aaa7fbc"
+    print(page_id)
+    response = requests.get(current_app.config["API_MONGO"], page_id)
+    curriculos = response.json()
+    return render_template('curriculo/list.html', curriculos=curriculos)
+
+
+@webapp.route('/novo')
+def create():
+    response = requests.post(
+        "https://curriculo-node.herokuapp.com/curriculos")
+    curriculos = response.json()
+    # print(curriculos)
+    return render_template('curriculo/list.html', curriculos=curriculos)
+
+
+@webapp.route('/editar')
+def edit():
     response = requests.get(
         "https://curriculo-node.herokuapp.com/curriculos")
     curriculos = response.json()
--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+"""Flask configuration."""
+from os import path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
 class Config(object):
     DEBUG = False
     TESTING = False
@@ -12,3 +20,4 @@ class DevelopmentConfig(Config):
     ENV = "development"
     DEVELOPMENT = True
     DEBUG = True
+    API_MONGO = "https://curriculo-node.herokuapp.com/curriculo"
