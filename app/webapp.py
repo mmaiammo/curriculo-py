6ab8e19769ee7a1a7daa77b410a90906d4a9e5eb
Maia <mmaia.inf@gmail.com>
2022-05-03 20:53:13 -0300

    Updates

--- app/webapp.py
+++ app/webapp.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2 import token
 import requests
 # type: ignore
 # import json
@@ -38,12 +39,19 @@ def create():
     return render_template('curriculo/list.html', curriculos=curriculos)
 
 
-@webapp.route('/editar')
-def edit():
-    response = requests.get(
-        "https://curriculo-node.herokuapp.com/curriculos")
-    curriculos = response.json()
-    return render_template('curriculo/list.html', curriculos=curriculos)
+@webapp.route('/edit/<string:id>')
+def edit(id):
+    data_login = {
+        "username": "day",
+        "password": "jean2004"
+    }
+    url = "https://curriculo-node.herokuapp.com/api/auth/signin"
+
+    response = requests.post(url=url, json=data_login)
+
+    data_res = response.json()
+    token = data_res['accessToken']
+    return render_template('curriculo/edit.html')
 
 
 @webapp.route('/sobre')
